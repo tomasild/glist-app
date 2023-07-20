366defb794366ea47e886f5229464abf472e1245
tomasild <tomas.ild.93@gmail.com>
2023-07-20 11:28:22 -0400

    probando react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import SideBar from "./components/SideBar";
 import Center from "./components/Center";
 import MusicPlayer from "./components/MusicPlayer";
+import Home from "./pages/Home";
+import AlbumList from './pages/AlbumList'
+
 
 function App() {
   return (
-    <div className="bg-black h-screen overflow-hidden">
+    <div className="bg-black h-screen">
       <main className="flex">
         <SideBar />
         <Center />
