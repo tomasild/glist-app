dd46cbebd2112a5d10400d205783b35a3752fd4e
tomasild <tomas.ild.93@gmail.com>
2023-07-31 13:04:15 -0400

    add album modify button

--- src/pages/AlbumDetails.jsx
+++ src/pages/AlbumDetails.jsx
@@ -9,6 +9,9 @@ function AlbumDetails() {
   const { albumId } = useParams();
   const [albumData, setAlbumData] = useState(null);
   const [albumSongs, setAlbumSongs] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState("");
+  const [editedYear, setEditedYear] = useState("");
 
   useEffect(() => {
     console.log("Obteniendo detalles del álbum desde el backend");
@@ -37,10 +40,39 @@ function AlbumDetails() {
     return date.toLocaleDateString("es-ES");
   };
 
+  const handleEditClick = () => {
+    setIsEditing(true);
+    setEditedTitle(albumData?.title || "");
+    setEditedYear(albumData?.year || "");
+  };
+
+  const handleCancelClick = () => {
+    setIsEditing(false);
+  };
+
+  const handleSaveClick = () => {
+    // Realizar la llamada a la API para actualizar el álbum
+    const updatedAlbum = {
+      title: editedTitle,
+      year: editedYear,
+    };
+
+    axios
+      .put(`http://localhost:3000/api/albums/${albumId}`, updatedAlbum)
+      .then((res) => {
+        // Actualizar los datos del álbum con la respuesta de la API
+        setAlbumData(res.data);
+        setIsEditing(false);
+      })
+      .catch((err) => {
+        console.error("Error al actualizar el álbum:", err);
+      });
+  };
+
   return (
     <div className="flex-grow text-white overflow-y-scroll h-screen scrollbar-hide animate-slowfade">
       <div className="w-full flex items-end space-x-7 bg-gradient-to-b to-black from-orange-950 h-64 text-white p-8">
-        {albumData ? (
+        {albumData && !isEditing ? (
           <>
             <img
               className="h-28 w-28 lg:w-44 lg:h-44 md:w-36 md:h-36 shadow-2xl rounded-xl animate-slowfade"
@@ -49,7 +81,10 @@ function AlbumDetails() {
             />
             <div className="text-white animate-slowfade">
               <div className="flex justify-start items-start mb-6 md:mb-8 xl:mb-10 space-x-4 text-gray-500 text-lg xl:text-xl">
-                <button className="hover:text-blue-500">
+                <button
+                  onClick={handleEditClick}
+                  className="hover:text-blue-500"
+                >
                   <RxUpdate />
                 </button>
                 <button className="hover:text-red-500">
@@ -57,31 +92,84 @@ function AlbumDetails() {
                 </button>
               </div>
               <div className="text-xs md:text-sm lg:text-lg">
-              <p>
-                <strong>{albumData.title}</strong>
-              </p>
-              <p>
-                <strong>Year: </strong>
-                {albumData.year}
-              </p>
-              <p>
-                <strong>Updated: </strong>
-                {formatDate(albumData.updatedAt)}
-              </p>
-              <p>
-                <strong>Created: </strong>
-                {formatDate(albumData.createdAt)}
-              </p>
+                <p>
+                  <strong>{albumData.title}</strong>
+                </p>
+                <p>
+                  <strong>Year: </strong>
+                  {albumData.year}
+                </p>
+                <p>
+                  <strong>Updated: </strong>
+                  {formatDate(albumData.updatedAt)}
+                </p>
+                <p>
+                  <strong>Created: </strong>
+                  {formatDate(albumData.createdAt)}
+                </p>
               </div>
             </div>
           </>
         ) : (
-          <p className="text-white m-5">Cargando detalles del álbum...</p>
+          <div className="text-white animate-slowfade">
+            <div className="text-xs md:text-sm lg:text-lg flex flex-col space-y-2">
+              <input
+                className="rounded-md bg-slate-600 p-1 text-xs xl:text-base"
+                type="text"
+                value={editedTitle}
+                onChange={(e) => setEditedTitle(e.target.value)}
+              />
+              <input
+                className="rounded-md bg-slate-600 p-1 text-xs xl:text-base"
+                type="number"
+                value={editedYear}
+                onChange={(e) => setEditedYear(e.target.value)}
+              />
+              {albumData && (
+                <div className="text-xs md:text-sm xl:text-base">
+                  <p>
+                    <strong>Updated: </strong>
+                    {formatDate(albumData.updatedAt)}
+                  </p>
+                  <p>
+                    <strong>Created: </strong>
+                    {formatDate(albumData.createdAt)}
+                  </p>
+                </div>
+              )}
+            </div>
+            <div className="flex justify-start items-start space-x-4 text-sm xl:text-lg pt-2 text-white font-semibold">
+              <button
+                onClick={handleSaveClick}
+                className="w-full px-1 rounded-sm bg-green-700 hover:bg-opacity-70"
+              >
+                Save
+              </button>
+              <button
+                onClick={handleCancelClick}
+                className="w-full px-1 rounded-sm bg-red-700 hover:bg-opacity-70"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
         )}
       </div>
-      <div className="grid m-4 px-4 py-2 text-white max-w-7xl">
+      <div className="grid m-4 px-4 py-2 text-white max-w-7xl animate-slideup">
         <h2 className="mb-2">Description</h2>
-        <p className="text-sm xl:text-base text-justify">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores minima dolores aspernatur sequi, quasi corporis repellendus praesentium sapiente delectus maxime! Labore, ad. Expedita dolorem dolorum voluptas ullam tempore tempora officia.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores minima dolores aspernatur sequi, quasi corporis repellendus praesentium sapiente delectus maxime! Labore, ad. Expedita dolorem dolorum voluptas ullam tempore tempora officia.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores minima dolores aspernatur sequi, quasi corporis repellendus praesentium sapiente delectus maxime! Labore, ad. Expedita dolorem dolorum voluptas ullam tempore tempora officia.</p>
+        <p className="text-sm xl:text-base text-justify">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+          minima dolores aspernatur sequi, quasi corporis repellendus
+          praesentium sapiente delectus maxime! Labore, ad. Expedita dolorem
+          dolorum voluptas ullam tempore tempora officia.Lorem, ipsum dolor sit
+          amet consectetur adipisicing elit. Asperiores minima dolores
+          aspernatur sequi, quasi corporis repellendus praesentium sapiente
+          delectus maxime! Labore, ad. Expedita dolorem dolorum voluptas ullam
+          tempore tempora officia.Lorem, ipsum dolor sit amet consectetur
+          adipisicing elit. Asperiores minima dolores aspernatur sequi, quasi
+          corporis repellendus praesentium sapiente delectus maxime! Labore, ad.
+          Expedita dolorem dolorum voluptas ullam tempore tempora officia.
+        </p>
       </div>
       <div className="text-white m-5 animate-slideup">
         <h2 className="mb-4 ml-2">Album songs</h2>
